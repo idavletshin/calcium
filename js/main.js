fd894a034e6273dc29578465b62d1a5c12f584e8
i.davletshin <i.davletshin14@gmail.com>
2021-09-17 13:23:06 +0300

    dfsdf

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// const burger = document.querySelector(".burger");
+const menuList = document.querySelector(".menu__list");
+const body = document.querySelector("body")
+const togglePasswordList = document.querySelectorAll(".text-field__icon");
+// const pastePassword = document.querySelector(".text-field__paste");
+const typeList = document.querySelectorAll(".password");
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    // This hides the address bar:
+    window.scrollTo(0, 1);
+  }, 0);
+});
+
+// burger.addEventListener('click', () => {
+//   burger.classList.toggle('active')
+//   menuList.classList.toggle('active')
+//   body.classList.toggle('--scroll-off')
+// })
+
+for (let i = 0; i < togglePasswordList.length; i++) {
+  togglePasswordList[i].addEventListener('click', function (event) {
+    const currentInput = this.parentNode.querySelector('.text-field__input');
+    const currentType = currentInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    currentInput.setAttribute('type', currentType);
+    this.classList.toggle("view");
+  });
+}
